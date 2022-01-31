80e896ad7669fdbedba7c35c7b516927fd767645
anshumalivfx <anshumali.karna99@gmail.com>
2022-01-31 21:06:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/anshumali/amusic/PlaySong.java
@@ -0,0 +1,57 @@
+package com.anshumali.amusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PlaySong extends AppCompatActivity {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+
+    TextView textView;
+    ImageView play, prev, next;
+    ArrayList<File> songs;
+    int position;
+
+    MediaPlayer mediaPlayer;
+
+    SeekBar seekBar;
+    Thread updateSeek;
+
+    String textContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_song);
+        textView = findViewById(R.id.textView);
+        play = findViewById(R.id.play);
+        prev = findViewById(R.id.prev);
+        next = findViewById(R.id.next);
+        seekBar = findViewById(R.id.seekBar);
+
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        songs = (ArrayList) bundle.getParcelableArrayList("songList");
+        textContent = intent.getStringExtra("currentSong");
+        textView.setText(textContent);
+        position = intent.getIntExtra("position", 0);
+        Uri uri = Uri.parse(songs.get(position).toString());
+        mediaPlayer = MediaPlayer.create(this, uri);
+        mediaPlayer.start();
+    }
+}
